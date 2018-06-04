308b6f714cb7f2e0c236375eb538e78624e31a1b
tanasiuk <dmitriy.tanasiuk@nixsolutions.com>
2018-06-04 19:08:04 +0300

    DB changes

--- src/main/java/ua/nure/tanasiuk/dao/mapper/UserIdentityRowMapper.java
+++ src/main/java/ua/nure/tanasiuk/dao/mapper/UserIdentityRowMapper.java
@@ -6,19 +6,15 @@ import ua.nure.tanasiuk.model.UserIdentity;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.Date;
 
 @Service
 public class UserIdentityRowMapper implements RowMapper<UserIdentity> {
 
     @Override
     public UserIdentity mapRow(ResultSet resultSet, int i) throws SQLException {
-        Timestamp birthdate = resultSet.getTimestamp("birthdate");
         return UserIdentity.builder()
             .id(resultSet.getLong("id"))
             .username(resultSet.getString("username"))
-            .birthdayDate(birthdate == null ? null : new Date(birthdate.getTime()))
             .email(resultSet.getString("email"))
             .password(resultSet.getString("password"))
             .build();
--- src/main/java/ua/nure/tanasiuk/model/UserIdentity.java
+++ src/main/java/ua/nure/tanasiuk/model/UserIdentity.java
@@ -1,13 +1,9 @@
 package ua.nure.tanasiuk.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ua.nure.tanasiuk.resource.deserializer.UnixDateDeserializer;
-import ua.nure.tanasiuk.resource.serializer.UnixDateSerializer;
 
 import java.util.Date;
 
@@ -18,9 +14,6 @@ import java.util.Date;
 public class UserIdentity {
     private Long id;
     private String username;
-    @JsonSerialize(using = UnixDateSerializer.class)
-    @JsonDeserialize(using = UnixDateDeserializer.class)
-    private Date birthdayDate;
     private String email;
     private String password;
     private Date creationDate;
